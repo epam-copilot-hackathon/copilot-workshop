5fd1fe51dece96b57579a257ba40c60731ce70d7
cleckit <robert_przydatek@epam.com>
2024-06-20 09:57:15 +0000

    ADD: [1/4] CheckDates endpoint is now reposinsive

--- exercisefiles/node/nodeserver.js
+++ exercisefiles/node/nodeserver.js
@@ -1,15 +1,5 @@
-// write a nodejs server that will expose a method call "get" that will return the value of the key passed in the query string
-// example: http://localhost:3000/get?key=hello
-// if the key is not passed, return "key not passed"
-// if the key is passed, return "hello" + key
-// if the url has other methods, return "method not supported"
-// when server is listening, log "server is listening on port 3000"
-
-
-const http = require('http');
 const url = require('url');
-
-
+const http = require('http');
 
 
 const server = http.createServer((req, res) => {
@@ -23,22 +13,18 @@ const server = http.createServer((req, res) => {
         } else {
             res.end(`hello ${key}`);
         }
+    } else if (pathname === '/DaysBetweenDates') {
+        const date1 = query.date1;
+        const date2 = query.date2;
+        if (!date1 || !date2) {
+            res.end('Both date1 and date2 parameters are required');
+        } else {
+            const daysBetween = Math.floor((new Date(date2) - new Date(date1)) / (1000 * 60 * 60 * 24));
+            res.end(`Days between ${date1} and ${date2}: ${daysBetween}`);
+        }
     } else {
         res.end('method not supported');
     }
-
-    if (pathname === '/DaysBetweenDates') {
-    const date1 = query.date1;
-    const date2 = query.date2;
-    if (!date1 || !date2) {
-        res.end('Both date1 and date2 parameters are required');
-    } else {
-        const daysBetween = Math.floor((new Date(date2) - new Date(date1)) / (1000 * 60 * 60 * 24));
-        res.end(`Days between ${date1} and ${date2}: ${daysBetween}`);
-    }
-}
-
-
 });
 
 server.listen(3000, () => {
