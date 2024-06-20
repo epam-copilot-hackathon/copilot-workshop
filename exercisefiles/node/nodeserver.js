3d84a3d631e73b3a538120afa53a14f13cd99a67
cleckit <robert_przydatek@epam.com>
2024-06-20 09:41:00 +0000

    ADD: step 0 - working Node server with node nodeserver.js

--- exercisefiles/node/nodeserver.js
+++ exercisefiles/node/nodeserver.js
@@ -5,3 +5,42 @@
 // if the url has other methods, return "method not supported"
 // when server is listening, log "server is listening on port 3000"
 
+
+const http = require('http');
+const url = require('url');
+
+
+
+
+const server = http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+    const { pathname, query } = parsedUrl;
+
+    if (pathname === '/get') {
+        const key = query.key;
+        if (!key) {
+            res.end('hello world');
+        } else {
+            res.end(`hello ${key}`);
+        }
+    } else {
+        res.end('method not supported');
+    }
+
+    if (pathname === '/DaysBetweenDates') {
+    const date1 = query.date1;
+    const date2 = query.date2;
+    if (!date1 || !date2) {
+        res.end('Both date1 and date2 parameters are required');
+    } else {
+        const daysBetween = Math.floor((new Date(date2) - new Date(date1)) / (1000 * 60 * 60 * 24));
+        res.end(`Days between ${date1} and ${date2}: ${daysBetween}`);
+    }
+}
+
+
+});
+
+server.listen(3000, () => {
+    console.log('server is listening on port 3000');
+});
